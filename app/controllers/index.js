47da111ff8456efd6635ee8b74a38eb211353a95
mjuniper <mjuniper@gmail.com>
2018-02-28 11:15:34 -0700

    build app

---
+++ app/controllers/index.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    doSearch () {
+      const q = this.get('q');
+      this.transitionToRoute('items', {
+        queryParams: { q }
+      });
+    }
+  }
+});
